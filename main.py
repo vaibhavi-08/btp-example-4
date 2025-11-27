5d22a1fb6810dfcf4aca63aaa2cddb3808f43936
vaibhavi <vaibhavim081203@gmail.com>
2025-11-27 15:21:35 +0530

    formatting fixed

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
 from math_utils import multiply, is_even
 
+
 def main():
     print("--- Running inside Docker ---")
-    
+
     val1, val2 = 6, 7
     prod = multiply(val1, val2)
     print(f"The product of {val1} and {val2} is {prod}")
-    
+
     check_num = 10
     print(f"Is {check_num} even? {is_even(check_num)}")
+
 
 if __name__ == "__main__":
     main()
--- math_utils.py
+++ math_utils.py
@@ -2,6 +2,7 @@ def multiply(a: int, b: int) -> int:
     """Multiplies two numbers."""
     return a * b
 
+
 def is_even(number: int) -> bool:
     """Checks if a number is even."""
     return number % 2 == 0
